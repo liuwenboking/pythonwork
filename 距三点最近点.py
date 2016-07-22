b5ddf5989853863dc92d5b9098ef00b7ce059989
蓝色海风 <liuwenbo_wenbo@163.com>
2016-07-22 15:53:20 +0800

    完善多点拟合并找中心点

--- 距三点最近点.py
+++ 距三点最近点.py
@@ -7,27 +7,99 @@ Created on Thu Jul 21 19:55:46 2016
 
 from matplotlib import pyplot as plt
 import numpy as np
+runNum = 0
 
-X=np.array([[1,3,2]])
-Y=np.array([[3,1,6]])
-A=np.array([[1,1],[3,1],[2,1]])
-B=np.array([[3],[1],[6]])
+#拟合
+def fitting(A,B):
+	AATN = np.linalg.inv(A.T.dot(A))
+	fac = AATN.dot(A.T).dot(B)
+	print "fac",fac
+	result = A.dot(fac)
+	result = result.reshape(1,len(B))
+	return result
+def drawPoints(X,Y):
+	for i in range(len(X)):
+		plt.plot(X[i],Y[i]  ,marker='o')
+
+def drawLine(X,Y):
+	plt.plot(X,Y,'r',marker='^') 
+
+def initFig():
+	plt.grid(True,color='r')
+	plt.xlim([-2,7])
+	plt.ylim([-2,7])
+	# plt.axis("equal")
+
+def getLefRightSumByPos(d,pos):
+	leftSum = 0
+	rightSum = 0
+	posNum = d[pos][0]
+	for i in range(pos):
+		leftSum += abs(posNum - d[i][0])
+
+	for i in range(pos,len(d)):
+		rightSum += abs(posNum - d[i][0])
+
+	return (leftSum,rightSum)
+
+def getMinDistancePoin(d,pos):
+	posNum = d[pos][0]
+	leftSum = 0
+	rightSum = 0
+	preDiff = None
+	prePos = pos
+	global runNum 
+	leftSum,rightSum = getLefRightSumByPos(d, pos)
+	diff = abs(leftSum - rightSum)
+	
+	while (diff < preDiff and pos >=0 and pos <len(d) and runNum<100):
+		print "diff,pos,prepos,prediff,lefsum,rightsum", diff,pos,prePos,preDiff,leftSum,rightSum
+		preDiff = diff
+		prePos = pos
+		if leftSum > rightSum:
+			pos -= 1
+			leftSum,rightSum=getLefRightSumByPos(d, pos)
+		else:
+			pos += 1
+			leftSum,rightSum=getLefRightSumByPos(d, pos)
+		diff = abs(leftSum - rightSum)
+		
+		runNum += 1
+		
+	else:
+		return prePos
 
 
-AATN = np.linalg.inv(A.T.dot(A))
 
-fac = AATN.dot(A.T).dot(B)
-result = A.dot(fac)
-result = result.reshape(1,3)
-for i in range(3):
-    plt.plot(X[0][i] ,Y[0][i]  ,marker='o')    
-for i in range(3):
-    plt.plot(X[0][i] -X[0][0],Y[0][i]-result[0][0]  ,marker='o')
+if __name__ =="__main__":
+	# 定义多多边形
+	X=np.array([1,2,3,4,5]).reshape(1,5)
+	Y=np.array([3,5,3,5,4]).reshape(1,5)
+	# 定义A
+	A=np.array([[1,1],[2,1],[3,1],[4,1],[5,1]])
+	# 定义B
+	B=np.array([[3],[5],[3],[5],[4]])
+	# 拟合后的结果
+	result = fitting(A,B)
+	drawPoints(X[0], Y[0])
+	# drawPoints(X[0] -X[0][0], Y[0]-result[0][0])
+	drawLine(X[0],result[0])
+	# drawLine(X[0] -X[0][0], result[0]-result[0][0])
 
-plt.plot(X[0] -X[0][0],result[0]-result[0][0] ,'r',marker='^') 
+	#计算各点在拟合线上的投影点
+	d=[]
+	for i in range(len(X[0])):
+		Bi=np.array([X[0][i] - X[0][0],Y[0][i] -result[0][0]] ).reshape(2,1)
+		Ai=np.array([X[0][1] - X[0][0],result[0][1]-result[0][0]]).reshape(2,1)
+		P=fitting(Ai,Bi)
+		d.append([P[0][0],P[0][1]])
+		# drawPoints([P[0][0]], [P[0][1]])
 
-plt.plot(X[0] ,result[0] ,'r',marker='^') 
-plt.xlim([-2,7])
-plt.ylim([-7,7])
-plt.grid(True,color='r')
-plt.show()
+	d.sort()
+	pointPos = getMinDistancePoin(d, len(d) /2 )
+	drawPoints([d[pointPos][0]+X[0][0]],[d[pointPos][1]+result[0][0]])
+	plt.text(d[pointPos][0]+X[0][0], d[pointPos][1]+result[0][0], 'CenterPoint')
+	# 只考虑一维就可以
+	initFig()
+
+	plt.show()
